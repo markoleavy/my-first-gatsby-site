f30811da8ee87a43f93a4ce08e80942a3854602d
Marco Olivi <marcoolivi@Marcos-MacBook-Pro.local>
2022-11-29 10:21:33 +0100

    end of lesson 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,14 +1,16 @@
 import * as React from "react";
-import { graphql } from "gatsby";
-import Layout from "../components/layout";
-import Seo from "../components/seo";
+import { Link, graphql } from "gatsby";
+import Layout from "../../components/layout";
+import Seo from "../../components/seo";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <aritcle key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+            <Link to={node.frontmatter.slug}>{node.frontmatter.title}</Link>
+          </h2>
           <p>Posted: {node.frontmatter.date}</p>
           <p>{node.excerpt}</p>
         </aritcle>
@@ -24,6 +26,7 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
         excerpt
---
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Layout from "../../components/layout";
+import { graphql } from "gatsby";
+import Seo from "../../components/seo";
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+    }
+  }`;
+
+  const BlogPost = ({ data, children }) => {
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      {children}
+    </Layout>
+  );
+};
+
+export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />
+
+export default BlogPost;
